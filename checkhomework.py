238a8570bb1a15fe742a4504b585de693fceed48
klkvr <62447812+klkvr@users.noreply.github.com>
2020-03-31 04:59:45 +0300

    added dict for accumulating hw

--- checkhomework.py
+++ checkhomework.py
@@ -10,15 +10,20 @@ user_id = 410821501
 s = requests.Session()
 l = s.post('https://app.eschool.center/ec-server/login', data=users_data[user_id]['login_data'])
 timee = int(time.time()) - 86400 * 30
-diary = requests.get('https://app.eschool.center/ec-server/student/diary?userId=' + str(users_data[user_id]['id']) + '&d1=' + str(timee * 1000), cookies=s.cookies).json()['lesson']
-bindiary = {}
-for i in diary:
+getdiary = requests.get('https://app.eschool.center/ec-server/student/diary?userId=' + str(users_data[user_id]['id']) + '&d1=' + str(timee * 1000), cookies=s.cookies).json()['lesson']
+diary = {}
+for i in getdiary:
     date = i['date'] // 1000
     unit = i['unit']['name']
-    if date not in bindiary:
-        bindiary[date] = []
-    if unit not in bindiary[date]:
-        bindiary[date][unit] = []
-    print(len(i['part']))
+    if date not in diary:
+        diary[date] = {}
+    if unit not in diary[date]:
+        diary[date][unit] = []
     for j in i['part']:
-        print(len(j['variant']), j['variant'])
+        diary[date][unit].extend([{'text': elem['text'], 'file': elem['file']} for elem in j['variant']])
+for date in diary:
+    print(str(date) + ':')
+    for unit in diary[date]:
+        print('    ' + unit + ':')
+        for dz in diary[date][unit]:
+            print('        ', dz)
