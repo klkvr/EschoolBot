d2fcc5a1a98dc1c2dc2c48c5517ae15018bf1a9a
klkvr <62447812+klkvr@users.noreply.github.com>
2020-03-31 22:05:14 +0300

    done without telegram

--- checkhomework.py
+++ checkhomework.py
@@ -1,5 +1,11 @@
-
 import requests, time, codecs, traceback
+
+
+def save_diary(diary):
+    f = codecs.open('prevdiary.txt', 'w')
+    print(diary, file=f)
+    f.close()
+
 
 users_data = {int(i.rstrip().split()[0]): {'login_data':
                                                {'username': i.rstrip().split()[1:-2],
@@ -12,27 +18,56 @@ for i in users_data:
         s += j + ' '
     s = s[:-1]
     users_data[i]['login_data']['username'] = s
-user_id = 410821501
+try:
+    prev_diary = eval(codecs.open('prevdiary.txt', 'r').readline().rstrip())
+except:
+    prev_diary = {}
 diary = {}
 for user in users_data:
     try:
-        s = requests.Session()
-        l = s.post('https://app.eschool.center/ec-server/login', data=users_data[user]['login_data'])
-        timee = (int(time.time()) // 86400) * 86400 - 86400 * 14
-        getdiary = requests.get('https://app.eschool.center/ec-server/student/diary?userId=' + str(users_data[user]['id']) + '&d1=' + str(timee * 1000) + '&d2=' + str((timee + 86400 * 14) * 1000), cookies=s.cookies).json()['lesson']
         diary[user] = {}
-        for i in getdiary:
-            date = i['date'] // 1000
-            unit = i['unit']['name']
-            if date not in diary:
-                diary[user][date] = {}
-            if unit not in diary[user][date]:
-                diary[user][date][unit] = []
-            for j in i['part']:
-                diary[user][date][unit].extend([{'text': elem['text'], 'file': elem['file']} for elem in j['variant']])
+        s = requests.Session()
+        enter = s.post('https://app.eschool.center/ec-server/login', data=users_data[user]['login_data'])
+        attempts = 0
+        while enter.status_code != 200 and attempts < 3:
+            time.sleep(1)
+            enter = s.post('https://app.eschool.center/ec-server/login', data=users_data[user]['login_data'])
+            attempts += 1
+
+        if enter.status_code == 200:
+            timee = (int(time.time()) // 86400) * 86400
+            getdiary = requests.get('https://app.eschool.center/ec-server/student/diary?userId=' + str(users_data[user]['id']) + '&d1=' + str(timee * 1000) + '&d2=' + str((timee + 86400 * 14) * 1000 - 1), cookies=s.cookies).json()['lesson']
+            print('https://app.eschool.center/ec-server/student/diary?userId=' + str(users_data[user]['id']) + '&d1=' + str(timee * 1000) + '&d2=' + str((timee + 86400 * 14) * 1000 - 1))
+            for i in getdiary:
+                date = i['date'] // 1000
+                unit = i['unit']['name']
+                if date not in diary[user]:
+                    diary[user][date] = {}
+                if unit not in diary[user][date]:
+                    diary[user][date][unit] = []
+                for j in i['part']:
+                    for elem in j['variant']:
+                        if 'text' in elem:
+                            diary[user][date][unit].append({'text': elem['text'], 'file': elem['file']})
+                        else:
+                            diary[user][date][unit].append({'text': '', 'file': elem['file']})
+            if user in prev_diary:
+                if diary[user] != prev_diary[user]:
+                    for date in diary[user]:
+                        if date in prev_diary[user]:
+                            for unit in diary[user][date]:
+                                if unit in prev_diary[user][date]:
+                                    for hw in diary[user][date][unit]:
+                                        if hw not in prev_diary[user][date][unit]:
+                                            print('new homework', unit, date, hw)
+                                else:
+                                    for hw in diary[user][date][unit]:
+                                        print('new homework', unit, date, hw)
+            prev_diary[user] = diary[user]
+            save_diary(prev_diary)
+        else:
+            print('login err', user)
     except:
         print('err', user)
         print(traceback.format_exc())
-f = codecs.open('prevhomework.txt', 'w')
-print(diary, file=f)
-f.close()
+save_diary(prev_diary)
