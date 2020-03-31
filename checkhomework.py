1236cf01689e2a44db0388bfde0b1ad785c20e3f
klkvr <62447812+klkvr@users.noreply.github.com>
2020-04-01 00:01:26 +0300

    added telegram messages

--- checkhomework.py
+++ checkhomework.py
@@ -1,4 +1,4 @@
-import requests, time, codecs, traceback
+import requests, time, codecs, traceback, telebot, os
 
 
 def save_diary(diary):
@@ -6,7 +6,11 @@ def save_diary(diary):
     print(diary, file=f)
     f.close()
 
+os.chdir('EschoolBot')
+bot = telebot.TeleBot(open('bothash.txt', 'r').readline().rstrip())
 
+week = {'Mon': 'Понедельник', 'Tue': 'Вторник', 'Wed': 'Среду', 'Thu': 'Четверг', 'Fri': 'Пятницу', 'Sat': 'Субботу', 'Sun': 'Воскресенье'}
+month = {'Mar': 'Марта', 'Apr': 'Апреля', 'May': 'Мая', 'Sep': 'Сентября', 'Nov': 'Ноября', 'Dec': 'Декабря', 'Jan': 'Января', 'Feb': 'Февраля'}
 users_data = {int(i.rstrip().split()[0]): {'login_data':
                                                {'username': i.rstrip().split()[1:-2],
                                                 'password': i.rstrip().split()[-2]},
@@ -59,9 +63,35 @@ for user in users_data:
                                 if unit in prev_diary[user][date]:
                                     for hw in diary[user][date][unit]:
                                         if hw not in prev_diary[user][date][unit]:
+                                            ti = time.ctime(date).split()[:3]
+                                            t = week[ti[0]] + ', ' + ti[2] + ' ' + month[ti[1]]
+                                            msg = 'Новое домашнее задание по предмету *' + unit + '* на ' + t + ':\n' + hw['text']
+                                            bot.send_message(user, msg, parse_mode="Markdown")
+                                            for file in hw['file']:
+                                                id = file['id']
+                                                name = f['fileName']
+                                                f = open(name, 'wb')
+                                                f.write(s.get('https://app.eschool.center/ec-server/files/' + str(id), cookies=s.cookies).content)
+                                                f.close()
+                                                bot.send_document(user, open(name, 'rb'))
+                                                os.remove(name)
                                             print('new homework', unit, date, hw)
                                 else:
                                     for hw in diary[user][date][unit]:
+                                        ti = time.ctime(date).split()[:3]
+                                        t = week[ti[0]] + ', ' + ti[2] + ' ' + month[ti[1]]
+                                        msg = 'Новое домашнее задание по предмету *' + unit + '* на ' + t + ':\n' + hw[
+                                            'text']
+                                        bot.send_message(user, msg, parse_mode="Markdown")
+                                        for file in hw['file']:
+                                            id = file['id']
+                                            name = f['fileName']
+                                            f = open(name, 'wb')
+                                            f.write(s.get('https://app.eschool.center/ec-server/files/' + str(id),
+                                                          cookies=s.cookies).content)
+                                            f.close()
+                                            bot.send_document(user, open(name, 'rb'))
+                                            os.remove(name)
                                         print('new homework', unit, date, hw)
             prev_diary[user] = diary[user]
             save_diary(prev_diary)
