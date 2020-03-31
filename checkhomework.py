7e2ec4daa465e6fbce923c85148b40f959e58c83
klkvr <62447812+klkvr@users.noreply.github.com>
2020-03-31 04:33:37 +0300

    start

---
+++ checkhomework.py
@@ -0,0 +1,24 @@
+
+import requests, time
+
+users_data = {int(i.rstrip().split()[0]): {'login_data':
+                                               {'username': i.rstrip().split()[1:-2],
+                                                'password': i.rstrip().split()[-2]},
+                                           'id': int(i.rstrip().split()[-1])} for i in open('pal.txt', 'r')}
+
+user_id = 410821501
+s = requests.Session()
+l = s.post('https://app.eschool.center/ec-server/login', data=users_data[user_id]['login_data'])
+timee = int(time.time()) - 86400 * 30
+diary = requests.get('https://app.eschool.center/ec-server/student/diary?userId=' + str(users_data[user_id]['id']) + '&d1=' + str(timee * 1000), cookies=s.cookies).json()['lesson']
+bindiary = {}
+for i in diary:
+    date = i['date'] // 1000
+    unit = i['unit']['name']
+    if date not in bindiary:
+        bindiary[date] = []
+    if unit not in bindiary[date]:
+        bindiary[date][unit] = []
+    print(len(i['part']))
+    for j in i['part']:
+        print(len(j['variant']), j['variant'])
