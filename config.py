f4544dc931bc96ca88ffce36ce1f487ebe78d723
klkvr <klkvrr@gmail.com>
2020-09-05 02:05:54 +0300

    .

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-os.chdir('/home/ubuntu/EschoolBot')
-
-BOT_HASH = "1016307061:AAF55OEGx5T2lTAcCovOO4x-xoqn3FOJoYQ"
-PERIOD = 204184
-WEEK = {'Mon': 'Понедельник', 'Tue': 'Вторник', 'Wed': 'Среду', 'Thu': 'Четверг', 'Fri': 'Пятницу', 'Sat': 'Субботу', 'Sun': 'Воскресенье'}
-MONTH = {'Mar': 'Марта', 'Apr': 'Апреля', 'May': 'Мая', 'Sep': 'Сентября', 'Nov': 'Ноября', 'Dec': 'Декабря', 'Jan': 'Января', 'Feb': 'Февраля'}
-LOG_CHANNEL = -1001212073907
-REDIS_DB = 5
