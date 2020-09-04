ba10d8816ef500efce0efceaa89266d1d23d0abc
klkvr <klkvrr@gmail.com>
2020-09-05 02:19:40 +0300

    .

--- send_marks.py
+++ send_marks.py
@@ -19,19 +19,20 @@ for user_id in users:
             if log_in_attempt['logged_in']:
                 s = log_in_attempt['session']
                 marks = user.get_marks(s)
-                time.sleep(0.5)
-                units = user.get_diary_units(s)
-                unit_by_id = {}
-                for unit in units:
-                    unit_by_id[unit['unit_id']] = unit['unit_name']
-                if user.last_checked_mark_time == -1:
-                    user.last_checked_mark_time = marks[-1]['time']
-                else:
-                    for mark in marks:
-                        if mark['time'] > user.last_checked_mark_time:
-                            user.last_checked_mark_time = mark["time"]
-                            msg = f'Новая оценка\n<b>{unit_by_id[mark["unit_id"]]}</b>\n<i>{mark["name"]}</i>\nЗначение: {mark["mark"]}\nКоэффициент: {mark["weight"]}'
-                            bot.send_message(user.id, msg, parse_mode="HTML")
+                if len(marks) != 0:
+                    time.sleep(0.5)
+                    units = user.get_diary_units(s)
+                    unit_by_id = {}
+                    for unit in units:
+                        unit_by_id[unit['unit_id']] = unit['unit_name']
+                    if user.last_checked_mark_time == -1:
+                        user.last_checked_mark_time = marks[-1]['time']
+                    else:
+                        for mark in marks:
+                            if mark['time'] > user.last_checked_mark_time:
+                                user.last_checked_mark_time = mark["time"]
+                                msg = f'Новая оценка\n<b>{unit_by_id[mark["unit_id"]]}</b>\n<i>{mark["name"]}</i>\nЗначение: {mark["mark"]}\nКоэффициент: {mark["weight"]}'
+                                bot.send_message(user.id, msg, parse_mode="HTML")
     except:
         bot.send_message('@eschool239boterrors', traceback.format_exc())
     time.sleep(0.5)
