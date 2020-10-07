cacee920c32c0d484d36a5cb440dfb6a66930afe
klkvr <klkvrr@gmail.com>
2020-10-08 00:17:12 +0300

    some func:

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import json
 import traceback
 import os
 import time
+from html2text import html2text
 from telegramcalendar import *
 from delete_html import *
 os.chdir('/home/ubuntu/EschoolBot')
@@ -99,7 +100,7 @@ class BotUser(object):
             for p in elem['part']:
                 if p['name'] == 'Дом. задание' or p['name'] == 'Контрольное д/з':
                     for variant in p['variant']:
-                        homeworks.append({'unit': unit, 'text': delete_html(variant.get('text', '')), 'files': [{'id': f['id'], 'name': f['fileName']} for f in variant['file']]})
+                        homeworks.append({'unit': unit, 'text': delete_html(html2text(variant.get('text', ''), bodywidth=0)), 'files': [{'id': f['id'], 'name': f['fileName']} for f in variant['file']]})
         return homeworks
 
 
