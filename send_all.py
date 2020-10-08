164ae90469885325a074b168a3f14238a6e3bcd0
klkvr <klkvrr@gmail.com>
2020-10-08 21:54:31 +0300

    notify types

---
+++ send_all.py
@@ -0,0 +1,16 @@
+import os
+os.chdir('/home/ubuntu/EschoolBot')
+
+from helpers import *
+from models import *
+
+TEST = 1
+msg = 'По многочисленным просьбам добавил функцию выбора оценок, о которых будут приходить уведомления. Можно получать все оценки, только хорошие, или не получать вообще.\n\nИспользуйте команду /notify_settings'
+
+if not TEST:
+    users = get_all_users()
+    for user_id in users:
+        bot.send_message(user_id, msg, parse_mode="HTML")
+else:
+    bot.send_message(410821501, msg, parse_mode="HTML")
+
