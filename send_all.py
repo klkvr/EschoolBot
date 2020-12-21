acf37578c0aef376f86585b7f4239fa966f7ce72
Arseniy Kulikov <klkvrr@gmail.com>
2020-12-21 23:44:49 +0300

    .

--- send_all.py
+++ send_all.py
@@ -8,7 +8,7 @@ import telebot
 
 bot = telebot.TeleBot(BOT_HASH)
 TEST = 0
-msg = 'на аве мой краш'
+msg = 'так ребята теперь это чудо мой проект по инфе, и мне будут ставить крутые оценки за то что я буду добавлять новые фичи, так что если у вас есть идея для новой функции в боте, то отпишите мне @klkvr'
 if not TEST:
     users = get_all_users()
     for user_id in users:
